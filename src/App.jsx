a60cbbe4c23ce6422b49bebfd96bceb5fb10a0da
Tuan Hai <tuanhai13112k3@gmail.com>
2025-12-23 13:21:00 +0700

    feat(form): react-hook-form and zod integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,46 @@
+import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from "react-hook-form";
+import { z } from 'zod';
 import './App.css';
 
+//1. Định nghĩa Schema
+const schema = z.object({
+  age: z.coerce.number({ invalid_type_error: "Tuổi phải là số" }).min(18, "Phải trên 18 tuổi").max(65, "Không được quá tuôi lao động cho phép"),
+  password: z.string().min(6, "Mặt khẩu tối thiểu 6 ký tự")
+})
 
 function App() {
   const {
     register, handleSubmit, formState: { errors }
-  } = useForm();
+  } = useForm({
+    resolver: zodResolver(schema) //2. Kết nối Resolver
+  });
   const onSubmit = (data) => console.log(data);
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)} className="p-4 space-y-4" >
         {/**Đăng ký input với validation cơ bản của HTMl */}
         <div>
-          <label>Tên đăng nhập:</label>
-          <input
-            {...register("username", { required: "Tên đăng nhập không được để trống" })}
+          <label>Age:</label>
+          <input type='number'
+            {...register("age", { required: "Tuổi không được để trống" })}
             className="border p-2"
+            placeholder='Nhập số tuổi'
           />
           {/**Hiển thị lỗi */}
-          {errors.username && <p className="text-red-500">{errors.username.message}</p>}
+          {errors.age && <p className="text-red-500">{errors.age.message}</p>}
         </div>
         <div>
-          <label>Email:</label>
+          <label>Password:</label>
           <input
-            {...register("email", {
-              required: "Email không được để trống",
-              pattern: {
-                value: /^\S+@\S+$/i,
-                message: "Email không đúng định dạng"
-              }
+            type='password'
+            {...register("password", {
+              required: "Mặt khẩu không được để trống",
             })}
+            placeholder='Password'
             className="border p-2"
           />
-          {errors.email && <p className="text-red-500">{errors.email.message}</p>}
+          {errors.password && <p className="text-red-500">{errors.password.message}</p>}
         </div>
         <button
           type="submit"
